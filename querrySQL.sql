ddc93ad8c2fb4ba0ec4b4d33e57d5285881a7f16
manuelvalverde <manel.valverde@hotmail.com>
2018-11-29 16:22:20 +0000

    querys finais

--- querrySQL.sql
+++ querrySQL.sql
@@ -1,3 +1,6 @@
+1-SELECT num_processo_socorro, COUNT(num_processo_socorro) FROM acciona GROUP BY num_processo_socorro HAVING COUNT(num_processo_socorro) = (SELECT MAX(C) FROM (SELECT COUNT(num_processo_socorro) AS C FROM acciona GROUP BY num_processo_socorro) AS Q); 
+
+
 2-SELECT nome_entidade FROM meio NATURAL JOIN audita WHERE data_hora_inicio>'2018-06-20 23:59:59' and data_hora_fim<'2018-09-24 00:00:00' and num_processo_socorro=(SELECT num_processo_socorro FROM (SELECT num_processo_socorro,COUNT(num_processo_socorro) AS max_count FROM audita GROUP BY num_processo_socorro ORDER BY max_count DESC LIMIT 1) AS max_processo);
 
 
@@ -8,5 +11,18 @@
 
 5-SELECT nome_meio FROM meio NATURAL JOIN acciona NATURAL JOIN meiocombate WHERE num_meio NOT IN (SELECT num_meio FROM meioapoio);
 
-6-
-
+6-SELECT nome_entidade
+FROM (SELECT DISTINCT nome_entidade, num_processo_socorro
+FROM acciona
+WHERE (num_meio, nome_entidade) IN 
+	(SELECT num_meio, nome_entidade
+	 FROM meiocombate
+	 WHERE (num_meio, nome_entidade) NOT IN
+	 	(SELECT num_meio, nome_entidade
+	 	 FROM alocado)
+	 and
+	 (num_meio, nome_entidade) NOT IN 
+	 	(SELECT num_meio, nome_entidade 
+	 	 FROM transporta))) As foo
+GROUP BY nome_entidade
+HAVING COUNT(num_processo_socorro) >= (SELECT Count(distinct num_processo_socorro) From acciona);
