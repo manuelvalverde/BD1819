79336b270690ce37fa0a8a39bb8713139c2848c1
Knalco <gongasgaspar97@gmail.com>
2018-11-30 16:12:28 +0000

    Fix os editings, por nos editings do sigma a parte do check dos veiculos

---
+++ editing/editingMS.php
@@ -0,0 +1,47 @@
+<html>
+    <body>
+<?php
+    $nome_meio = $_REQUEST['nome_meio'];
+    $num_meio = $_REQUEST['num_meio'];
+    $nome_entidade= $_REQUEST['nome_entidade'];
+    try
+    {
+ $host = "db.ist.utl.pt";
+        $user ="ist424774";
+        $password = "mv1pnmdq2Av";
+        $port=5432;
+        $dbname = $user;
+        $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $connection = pg_connect("host=$host port=$port user=$user password=$password dbname=$dbname") or die(pg_last_error());
+        
+        $sql = "SELECT num_meio,nome_entidade FROM meioapoio WHERE num_meio=$num_meio and nome_entidade='$nome_entidade';";
+        $result = pg_query($sql) or die('ERROR: ' . pg_last_error());
+        $row = pg_fetch_assoc($result);
+        $num_meio_check=$row["num_meio"];
+        $nome_entidade_check=$row["nome_entidade"];
+        if($num_meio_check==$num_meio && $nome_entidade_check=$nome_entidade){
+            $sql = "UPDATE meio SET nome_meio = :nome_meio WHERE num_meio=:num_meio and nome_entidade=:nome_entidade and :num_meio IN (SELECT num_meio FROM meioapoio) and :nome_entidade IN (SELECT nome_entidade FROM meioapoio);";
+            
+            $result = $db->prepare($sql);
+            $result->execute([':nome_meio' => $nome_meio,':num_meio' => $num_meio,':nome_entidade' => $nome_entidade]);
+        }else{throw new Exception("The vehicle given is not an Aid vehicle");}
+        echo("<p>$sql</p>");
+
+        pg_close($connection);
+        
+        
+        $db = null;
+    }
+    catch (PDOException $e)
+    {
+        echo("<p>ERROR: {$e->getMessage()}</p>");
+    }
+    catch (Exception $e)
+    {
+        echo("<p>ERROR: {$e->getMessage()}</p>");
+    }
+?>
+    </body>
+</html>
